477b9833ad07ef2a9a85ee95504ac27d01768106
Jamie Stewart <stewart.jamie.d@gmail.com>
2023-12-12 10:45:17 +0000

    Day 11 - I wasn't happy with the implementation or the speed at which it operated. Improved this to now run at around 0.004 seconds for both parts

--- source/day_11.cpp
+++ source/day_11.cpp
@@ -14,6 +14,7 @@
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <numeric>
 #include <string>
 #include <vector>
 
@@ -25,114 +26,107 @@ namespace day_11
 		uint64_t y;
 	};
 
-	std::vector<Galaxy_Location> read_input_file( const std::string& input_file )
+	struct Input_Map
+	{
+		std::vector<Galaxy_Location> locations;
+		std::vector<uint64_t> x_offsets;
+		std::vector<uint64_t> y_offsets;
+		
+	};
+
+	Input_Map read_input_file( const std::string& input_file )
 	{
 		std::vector<Galaxy_Location> galaxy_locations = {};
-		
+		std::vector<uint64_t> x_offsets = {};
+		std::vector<uint64_t> y_offsets = {};
+
 		//file is a 2D map of x by y with symbols '#' for galaxies and '.' for empty space
 		std::ifstream file_input;
 		file_input.open( input_file );
 		if ( file_input.is_open() )
 		{
-			uint64_t y_position = 0;
+			uint64_t y_position = {};
+			uint64_t y_offset = {};
+			std::string blank_columns = {};
 			for ( std::string line; std::getline( file_input, line ); )
 			{
 				if ( line.length() > 0 )
 				{
+					//if blank_columns is empty initialize it
+					if( blank_columns.empty() )
+					{
+						//set a string to be entirely blank each time a glalaxy is found fill in the location a x with '#'
+						blank_columns = std::string( line.size(), '.' );
+					}
 					//find the first galaxy
 					size_t sp = line.find_first_of( '#' );
+					if( sp == std::string::npos )
+					{
+						//each time we have blank line increment the y offset 
+						++y_offset;
+					}
+					y_offsets.push_back( y_offset );
 					while ( sp != std::string::npos )
 					{
+						blank_columns[sp] = '#';
 						galaxy_locations.push_back( { sp, y_position } );
 						sp = line.find_first_of( '#', sp+1 );						
 					}
 					++y_position;
 				}
 			}
+			//figure out which columns are still empty
+			uint64_t x_offset = {};
+			for ( const auto& c : blank_columns )
+			{
+				if( c == '.' )
+				{
+					++x_offset;
+				}
+				x_offsets.push_back( x_offset );
+			}
 			file_input.close();
-		}		
-		return galaxy_locations;
-	}
-
-	void adjust_position( std::vector<Galaxy_Location>& galaxy_locations, const uint64_t offset_distance, const bool adjust_x )
-	{
-		//iterate over galaxies if there is a difference of more than 1 between x position then increment x values
-		uint64_t offset = {};
-		// var explanation: cl == current_location, pl == previous_location
-		uint64_t pl = {};
-		for ( auto& [x, y] : galaxy_locations )
-		{
-			uint64_t cl = (adjust_x) ? x + offset : y + offset;
-			if ( const uint64_t diff = std::max( cl, pl ) - std::min( cl, pl );  diff > 1 )
-			{
-				const uint64_t displacement =(diff - 1) * (offset_distance - 1);
-				offset += displacement;
-				cl += displacement;
-			}
-			pl = cl;
-			if( adjust_x)
-			{
-				x = cl;
-			}
-			else
-			{
-				y = cl;
-			}
 		}
+
+		//we now have a vector of all galaxy locations and two vectors of offset amounts in x and y directions
+		//offset vectors are size of width and height of the galaxy map read in.
+		return { galaxy_locations, x_offsets, y_offsets };
 	}
 
-	std::vector<Galaxy_Location> adjust_for_empty_space( const std::vector<Galaxy_Location>& galaxy_locations, const uint64_t offset_distance )
+	uint64_t accumulate_distances( const Input_Map& input_map, uint64_t offset )
 	{
-		std::vector<Galaxy_Location> adjusted_galaxies = galaxy_locations;
-		//need to adjust for empty column spaces
-		std::ranges::sort( adjusted_galaxies.begin(), adjusted_galaxies.end(), []( const Galaxy_Location& a, const Galaxy_Location& b )
-						   {
-								//sort based on x location
-								return a.x < b.x;
-						   } );
-		adjust_position( adjusted_galaxies, offset_distance, true );
-
-		//need to adjust for empty row spaces
-		std::ranges::sort( adjusted_galaxies.begin(), adjusted_galaxies.end(), []( const Galaxy_Location& a, const Galaxy_Location& b )
-						   {
-							   //sort based on x location
-							   return a.y < b.y;
-						   } );
-		adjust_position( adjusted_galaxies, offset_distance, false );
-
-		return adjusted_galaxies;
-	}
-
-	uint64_t accumulate_distances( const std::vector<Galaxy_Location>& locations )
-	{
-		uint64_t accumulated_distance = {};
+		std::vector<uint64_t> distances = {};
+		const auto& locations = input_map.locations;
 		for ( auto iter = locations.begin(); iter != locations.end(); ++iter )
 		{
-			const Galaxy_Location first = *iter;
-			for ( auto inner_iter = iter + 1; inner_iter != locations.end(); ++inner_iter )
-			{
-				const Galaxy_Location second = *inner_iter;
-				const uint64_t x_dist = std::max( first.x, second.x ) - std::min( first.x, second.x );
-				const uint64_t y_dist = std::max( first.y, second.y ) - std::min( first.y, second.y );
-				accumulated_distance += (x_dist + y_dist);
-			}
-		}
-		return accumulated_distance;
+			Galaxy_Location first = *iter;
+			//get the actual position of the galaxy with the x/y offsets applied 
+			first.x += (input_map.x_offsets[first.x] * offset);
+			first.y += (input_map.y_offsets[first.y] * offset);
+
+			std::transform( iter + 1, locations.end(), std::back_inserter( distances ),
+							[&first, &input_map, offset ]( Galaxy_Location gl )
+							{
+								//get the actual position of the galaxy with offsets applied from look up
+								gl.x += (input_map.x_offsets[gl.x] * offset);
+								gl.y += (input_map.y_offsets[gl.y] * offset);
+								const uint64_t x_dist = std::max( first.x, gl.x ) - std::min( first.x, gl.x );
+								const uint64_t y_dist = std::max( first.y, gl.y ) - std::min( first.y, gl.y );
+								return x_dist + y_dist;
+							} );
+		}			
+		return std::accumulate( distances.begin(), distances.end(),0LLU );
 	}
 	
-	uint64_t part_01( std::vector<Galaxy_Location>& galaxy_locations )
+	uint64_t part_01( const Input_Map& input_map )
 	{
-		//adjust galaxies for an offset space of 1
-		const std::vector<Galaxy_Location> adjusted_galaxies = adjust_for_empty_space( galaxy_locations, 2LLU );
-		return accumulate_distances( adjusted_galaxies );
+		return accumulate_distances( input_map, 1 );
 		
 	}
 
-	uint64_t part_02( std::vector<Galaxy_Location>& galaxy_locations )
+	uint64_t part_02( const Input_Map& input_map )
 	{
-		//adjust galaxies for an offset space of 1
-		const std::vector<Galaxy_Location> adjusted_galaxies = adjust_for_empty_space( galaxy_locations, 1000000LLU );
-		return accumulate_distances( adjusted_galaxies );
+		return accumulate_distances( input_map, 999999 );
 	}
 
 
@@ -141,7 +135,7 @@ namespace day_11
 Result aoc::day_11()
 {
 	timer::start();
-	std::vector<day_11::Galaxy_Location> galaxy_locations = day_11::read_input_file( "./data/day_11_input.txt" );
+	day_11::Input_Map galaxy_locations = day_11::read_input_file( "./data/day_11_input.txt" );
 
 	const uint64_t part_1_answer = day_11::part_01( galaxy_locations );
 
