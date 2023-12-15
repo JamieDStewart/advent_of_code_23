9b883bff8fd84768f0470ffddbd9eab02cfac765
Jamie Stewart <stewart.jamie.d@gmail.com>
2023-12-15 15:52:38 +0000

    realised i could improve the performance of day 1

--- source/day_01.cpp
+++ source/day_01.cpp
@@ -17,13 +17,24 @@
 #include <string>
 #include <vector>
 
+/*
+ * Creating a namespace in this file to add in any functions used in solving the problem
+ * I tend to use a mixture of C/C++ styling when solving Advent of code problems as it's
+ * easy for me to work this way and keep each problem in a separate file without worrying
+ * about any function name clashes between days.
+ */
 namespace day_01
 {
-	void read_input_from_file( std::vector<std::string>& input_vector )
+	/**
+	/* @brief a function to read input from file and store each input line as a string in a vector
+	 * @param input_file the directory path and name of the file to read from
+	*/
+	std::vector<std::string> read_input_from_file( const std::string& input_file )
 	{
+		std::vector<std::string> input_vector = {};
 		//open the input file for reading
 		std::ifstream file_input;
-		file_input.open( "./input/day_01.txt" );
+		file_input.open( input_file );
 		if ( file_input.is_open() )
 		{
 			for ( std::string line; std::getline( file_input, line ); )
@@ -34,88 +45,67 @@ namespace day_01
 				}
 
 			}
-			//close the input file
 			file_input.close();
 		}
+		return input_vector;
 	}
 
+	/**
+	 * @brief function to solve part 1 of the problem for day 1.
+	 *        Solution requires finding the first and last digit in the string
+	 *        and adding these to a running total 
+	 * @param input a vector of strings that contain numerical digits
+	 * @return the sum total of all combined first & last digits in the input string vector
+	 */
 	uint32_t part_1( std::vector<std::string>& input )
 	{
 		//keep track of the accumulated total for the calibration values
-		uint32_t calibration_value_sum = 0;
-		uint32_t calibration_value = 0;
+		uint32_t calibration_value_sum = {};
+		uint32_t calibration_value = {};
 		const std::string digits = "0123456789";
 
 		for ( const auto& line : input )
 		{
-			//find the first integer value in the string
-			const size_t first = line.find_first_of( digits );
-			const size_t last = line.find_last_of( digits );
-			if ( first != std::string::npos )
-			{
-				const std::string calibration_value_string = std::string( line, first, 1 ) + std::string( line, last, 1 );
-				std::stringstream ss( calibration_value_string );
-				ss >> calibration_value;
-				calibration_value_sum += calibration_value;
-			}
-		}
+			const size_t first = line.find_first_of( digits );		//find the first integer value in the string
+			const size_t last = line.find_last_of( digits );			//find the last integer value in the string
 
+			const std::string calibration_value_string = std::string( line, first, 1 ) + std::string( line, last, 1 );
+			std::stringstream ss( calibration_value_string );
+			ss >> calibration_value;
+			calibration_value_sum += calibration_value;
+			
+		}
 		return calibration_value_sum;
 	}
 
+	/**
+	 * @brief Function to solve part 2 of the question
+	 *        This time the input needs to be scanned for the written number values as well as the decimal values
+	 * @param input vector of strings that contain written and decimal number values
+	 * @return the sum total of all combined first and last numbers found in each input string from the input vector
+	 */
 	uint32_t part_2( std::vector<std::string>& input )
 	{
-		struct value_pos
-		{
-			std::string value;
-			size_t pos;
-		};
-
-		//keep track of the accumulated total for the calibration values
-		uint32_t calibration_value_sum = 0;
-		uint32_t calibration_value = 0;
-		const std::string digits = "0123456789";
-
 		const std::vector<std::string> search_strings = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-		const std::vector<std::string> search_values = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+		const std::vector<char> search_values = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
-		for ( const auto& line : input )
+		for ( auto& line : input )
 		{
-			//find the positions of the first integer value in the string
-			const size_t first = line.find_first_of( digits );
-			const size_t last = line.find_last_of( digits );
-			//search the string and find occurrences of the number strings
-			std::vector<value_pos> locations;
-			for ( int i = 0; i < search_strings.size(); ++i )
+			uint32_t index = 0;
+			for ( auto& number : search_strings )
 			{
-				size_t offset = 0;
-				size_t location = 0;
-				//perform this as a while loop so that strings containing multiple sub stings of number are all accounted for eg. two43two
-				while ( location != std::string::npos )
+				size_t location = line.find( number );
+				while( location != std::string::npos )
 				{
-					location = line.find( search_strings[i], location + offset );
-					if ( location != std::string::npos )
-					{
-						offset = 1;
-						locations.push_back( { search_values[i], location } );
-					}
+					//replace the second character of this string with digit for that string
+					line[location+1] = search_values[index];
+					location = line.find( number, location+1 );
 				}
-			}
-			//sort locations based on position in string
-			std::sort( locations.begin(), locations.end(), []( const value_pos a, const value_pos b )
-					   {
-						   return (a.pos < b.pos);
-					   } );
-
-			std::string first_digit = (!locations.empty() && locations[0].pos < first) ? locations[0].value : std::string( line, first, 1 );
-			std::string last_digit = (last == std::string::npos || (!locations.empty() && locations[locations.size() - 1].pos > last)) ? locations[locations.size() - 1].value : std::string( line, last, 1 );
-
-			std::stringstream ss( first_digit + last_digit );
-			ss >> calibration_value;
-			calibration_value_sum += calibration_value;
+				++index;
+			}			
 		}
 
-		return calibration_value_sum;
+		return part_1(input );
 	}
 }
 
@@ -123,9 +113,7 @@ Result aoc::day_01()
 {
 	timer::start();
 	//vector to store the total quantity of calories that an elf is carrying
-	std::vector< std::string > input_values;
-	//read file input
-	day_01::read_input_from_file( input_values );
+	std::vector< std::string > input_values = day_01::read_input_from_file( "./input/day_01.txt" );
 
 	const uint32_t part_1_calibration_values_sum = day_01::part_1( input_values );
 	const uint32_t part_2_calibration_values_sum = day_01::part_2( input_values );
